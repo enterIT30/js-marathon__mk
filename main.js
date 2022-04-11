01627079b2a524dcfd40e8526023c74307135026
Vladyslav <ryzhykovvv@gmail.com>
2022-04-11 22:23:50 +0300

    home work 5 11.04

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const player1 = {
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
   weapon: ['kunai', 'sword', 'fire'],
-  attack: console.log('Scorpion' + ' Fight...'),
-  changeHP: changeHP,
-  elHP: elHP,
-  renderHP:renderHP,
+  attack,
+  changeHP,
+  elHP,
+  renderHP,
 };
 
 const player2 = {
@@ -21,10 +21,14 @@ const player2 = {
   img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
   weapon: ['sword', 'axe', 'ice'],
   attack: console.log('Sub-Zero' + ' Fight...'),
-  changeHP: changeHP,
-  elHP: elHP,
-  renderHP:renderHP,
+  changeHP,
+  elHP,
+  renderHP,
 };
+
+function attack() {
+  console.log(this.name + ' ' + 'Fight...');
+}
 
 
 function createElement(tag, className) {
@@ -58,14 +62,8 @@ function createPlayer(player) {
   return players;
 }
 
-//==================================================================================
-
 
 function changeHP(random) {
-
-  //!=======================================
-  console.log(this.hp);
-
   if (this.hp >= 1) {
     this.hp -= random;
   }
@@ -75,30 +73,13 @@ function changeHP(random) {
   }
 }
 
-//? Первый вариант всё в функции elHP (закоментировать renderHP:renderHP,)
-
-/* function elHP() {
-  let life = document.querySelector('.player'+ this.player +' .life');
-  life.style.width = this.hp + '%';
-  return life;
-}
- */
-//?===========================================
-
-
-//? Вторй вариант с elHP и renderHP, но с вызовом player1.renderHP(player1.elHP());
-
 function elHP() {
-  let life = document.querySelector('.player'+ this.player +' .life');
-  return life;
+  return document.querySelector('.player'+ this.player +' .life');
 }
 
-function renderHP(life) {
-  life.style.width = this.hp + '%';
+function renderHP() {
+  this.elHP().style.width = this.hp + '%';
 }
-
-//?===========================================
-
 
 function playerWins(name) {
   const loseTitle = createElement('div', 'winsTitle');
@@ -111,42 +92,36 @@ function playerWins(name) {
   return loseTitle;
 }
 
-function getRandom(number) {
-  return Math.ceil(Math.random() * number);
+function getRandom(num) {
+  return Math.ceil(Math.random() * num);
 }
 
 
 function createReloadButtom() {
   const reloadWrap = createElement('div', 'reloadWrap');
   const buttonRestart = createElement('button', 'button');
-
   buttonRestart.innerText = 'Restart';
-
-  reloadWrap.appendChild(buttonRestart);
 
   buttonRestart.addEventListener("click", function () {
     window.location.reload();
   });
 
-  return reloadWrap;
+  reloadWrap.appendChild(buttonRestart);
+  arenas.appendChild(reloadWrap);
 }
 
 
-randomButton.addEventListener("click", function() {
+randomButton.addEventListener('click', function() {
   player1.changeHP(getRandom(20));
   player2.changeHP(getRandom(20));
 
-  //!===============================================
-
-  player1.renderHP(player1.elHP());
-  player2.renderHP(player2.elHP());
-
-  //!===============================================
+  player1.renderHP();
+  player2.renderHP();
 
   if (player1.hp === 0 || player2.hp === 0) {
     randomButton.disabled = true;
     //randomButton.style.display = 'none';
-    arenas.appendChild(createReloadButtom());
+    createReloadButtom();
   }
 
   if (player1.hp === 0 && player1.hp < player2.hp) {
